bf7290771cf24fbf6a2525c0a1f68da7f6e545bb
Kevin Chen <kyc2915@gmail.com>
2021-12-27 18:47:13 -0600

    fix hashikake

--- solvers/hashiwokakero.py
+++ solvers/hashiwokakero.py
@@ -37,34 +37,45 @@ class HashiwokakeroSolver(AbstractSolver):
     def configure(self, sg):
         # Neighbor graph is symmetric and has no self-edges
         for edge in sg.lattice.points:
-            if edge.x == edge.y:
-                sg.solver.add(sg.cell_is(edge, 0))
-            else:
+            if edge.x < edge.y:
                 sg.solver.add(sg.grid[edge] == sg.grid[Point(edge.x, edge.y)])
+
+        # Only orthogonally adjacent islands can have bridges
+        edges = []
+        for edge in sg.lattice.points:
+            p, q = self.number_positions[edge.x], self.number_positions[edge.y]
+            if edge.x == edge.y:
+                pass
+            elif p.y == q.y and all(not self.grid[p.y][x].isnumeric() for x in range(min(p.x, q.x) + 1, max(p.x, q.x))):
+                edges.append(edge)
+            elif p.x == q.x and all(not self.grid[y][p.x].isnumeric() for y in range(min(p.y, q.y) + 1, max(p.y, q.y))):
+                edges.append(edge)
+            else:
+                sg.solver.add(sg.cell_is(edge, 0))
 
         # Each island has the correct number of bridges connected to it
         for i, p in enumerate(self.number_positions):
-            sg.solver.add(Sum(
-                [sg.grid[Point(i, j)] for j in range(self.num_numbers)]) == int(self.grid[p.y][p.x]))
+            sg.solver.add(Sum([sg.grid[edge] for edge in edges if i == edge.x]) == int(self.grid[p.y][p.x]))
 
-        # Any two islands not orthogonally adjacent cannot have bridges between them
-        for edge in sg.lattice.points:
-            p, q = self.number_positions[edge.x], self.number_positions[edge.y]
-            if p.y == q.y and all(not self.grid[p.y][x].isnumeric() for x in range(min(p.x, q.x) + 1, max(p.x, q.x))):
-                continue
-            if p.x == q.x and all(not self.grid[y][p.x].isnumeric() for y in range(min(p.y, q.y) + 1, max(p.y, q.y))):
-                continue
-            sg.solver.add(sg.cell_is(edge, 0))
+        # Bridges cannot intersect
+        for edge1 in edges:
+            for edge2 in edges:
+                p1, q1 = self.number_positions[edge1.x], self.number_positions[edge1.y]
+                p2, q2 = self.number_positions[edge2.x], self.number_positions[edge2.y]
+                if p1.x == q1.x and p2.y == q2.y and p1.y < p2.y < q1.y and p2.x < p1.x < q2.x:
+                    sg.solver.add(Or(sg.cell_is(edge1, 0), sg.cell_is(edge2, 0)))
 
         # Bridges are all connected
-        graph = SymbolGrid(
-            self.lattice(), grilops.make_number_range_symbol_set(0, self.num_numbers), sg.solver)
-        sg.solver.add(PbEq([(graph.cell_is(edge, 0), 1) for edge in sg.lattice.points], 1))
-        for edge in sg.lattice.points:
+        graph = defaultdict(lambda: Int(str(uuid4())))
+        sg.solver.add(PbEq([(graph[edge] == 0, 1) for edge in edges if edge.x < edge.y], 1))
+        for edge in edges:
+            sg.solver.add(graph[edge] >= 0)
+            sg.solver.add(graph[edge] < self.num_numbers)
+            sg.solver.add(graph[edge] == graph[Point(edge.x, edge.y)])
             sg.solver.add(Or(
                 sg.cell_is(edge, 0),
-                graph.cell_is(edge, 0),
-                *[And(graph.grid[edge] > graph.grid[Point(edge.x, y)], sg.grid[Point(edge.x, y)] != 0)
-                  for y in range(self.num_numbers)],
-                *[And(graph.grid[edge] > graph.grid[Point(x, edge.y)], sg.grid[Point(x, edge.y)] != 0)
-                  for x in range(self.num_numbers)]))
+                graph[edge] == 0,
+                *[And(graph[edge] > graph[other_edge], sg.grid[other_edge] != 0)
+                  for other_edge in edges if other_edge.x == edge.x],
+                *[And(graph[edge] > graph[other_edge], sg.grid[other_edge] != 0)
+                  for other_edge in edges if other_edge.y == edge.y]))
