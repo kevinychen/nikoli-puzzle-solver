b950f11e448f816a6333103c9caa69aeb650e55b
Kevin Chen <kyc2915@gmail.com>
2022-12-09 15:47:48 -0800

    add heyawake

--- solvers/heyawake.py
+++ solvers/heyawake.py
@@ -1,52 +1,35 @@
 from solvers.utils import *
 
 
-class HeyawakeSolver(AbstractSolver):
+class Heyawake(AbstractSolver):
 
-    def __init__(self, pzprv3):
-        matched = match('pzprv3/heyawake/(\\d+)/(\\d+)/(\\d+)/(.*)/', pzprv3)
-        self.height = int(matched.group(1))
-        self.width = int(matched.group(2))
-        self.num_regions = int(matched.group(3))
-        parsed_table = parse_table(matched.group(4))
-        self.regions = parsed_table[:self.height]
-        self.nums = parsed_table[self.height:2 * self.height]
-
-    def to_pzprv3(self, solved_grid):
-        symbol_set = self.symbol_set()
-        result = [[symbol_set.symbols[solved_grid[Point(row, col)]].label
-                   for col in range(self.width)] for row in range(self.height)]
-        return (
-            f'pzprv3/heyawake/'
-            f'{self.height}/{self.width}/{self.num_regions}/{table(self.regions)}/{table(self.nums)}/{table(result)}/')
-
-    def lattice(self):
-        return grilops.get_rectangle_lattice(self.height, self.width)
-
-    def symbol_set(self):
-        return SymbolSet([("WHITE", "+"), ("BLACK", "#")])
-
-    def configure(self, sg):
-        symbol_set = self.symbol_set()
+    def configure(self, puzzle, init_symbol_grid):
+        sg = init_symbol_grid(
+            grilops.get_rectangle_lattice(puzzle.height, puzzle.width),
+            grilops.make_number_range_symbol_set(0, 1))
         rc = RegionConstrainer(sg.lattice, sg.solver)
 
+        regions = dict([(p, i) for i, region in enumerate(puzzle.to_regions(sg.lattice.points)) for p in region])
+
         # No "word", i.e. line of white squares visiting at least three regions
-        for v in Vector(0, 1), Vector(1, 0):
-            for p in sg.lattice.points:
+        for v in Directions.E, Directions.S:
+            for p in sg.grid:
                 word = []
                 while p in sg.grid:
                     word.append(p)
-                    if len(set(self.regions[q.y][q.x] for q in word)) >= 3:
-                        sg.solver.add(Or([sg.cell_is(q, symbol_set.BLACK) for q in word]))
+                    if len(set(regions[q] for q in word)) >= 3:
+                        sg.solver.add(Or([sg.cell_is(q, 1) for q in word]))
                         break
                     p = p.translate(v)
 
         # Number of black squares in each region is correct
-        for p in sg.lattice.points:
-            num = self.nums[p.y][p.x]
-            if num.isnumeric():
-                sg.solver.add(Sum([sg.grid[q] for q in sg.lattice.points
-                                   if self.regions[q.y][q.x] == self.regions[p.y][p.x]]) == int(num))
+        for p, text in puzzle.texts.items():
+            sg.solver.add(Sum([sg.grid[q] for q in sg.lattice.points if regions[q] == regions[p]]) == text)
 
-        continuous_region(sg, rc, lambda q: sg.cell_is(q, symbol_set.WHITE))
-        no_adjacent_symbols(sg, symbol_set.BLACK)
+        continuous_region(sg, rc, lambda q: sg.cell_is(q, 0))
+        no_adjacent_symbols(sg, 1)
+
+    def set_solved(self, puzzle, sg, solved_grid, solution):
+        for p in sg.grid:
+            if solved_grid[p] == 1:
+                solution.shaded[p] = True
