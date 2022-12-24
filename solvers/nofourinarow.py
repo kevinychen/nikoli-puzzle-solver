1ab1e7d44de3307f5bb786db589d3f54d548e810
Kevin Chen <kyc2915@gmail.com>
2022-12-23 18:30:50 -0800

    No Four in a Row

---
+++ solvers/nofourinarow.py
@@ -0,0 +1,26 @@
+from lib import *
+
+
+class NoFourInARow(AbstractSolver):
+    def run(self, puzzle, solve):
+        symbols = list(set(symbol for symbol in puzzle.symbols.values()))
+
+        sg = SymbolGrid(puzzle.lattice(), grilops.make_number_range_symbol_set(0, 1))
+
+        for p, symbol in puzzle.symbols.items():
+            sg.solver.add(sg.grid[p] == symbols.index(symbol))
+
+        for p in sg.grid:
+            for v in sg.lattice.vertex_sharing_directions():
+                p2 = p.translate(v)
+                p3 = p2.translate(v)
+                p4 = p3.translate(v)
+                sg.solver.add(
+                    Or(sg.grid[p] != sg.grid.get(p2), sg.grid[p] != sg.grid.get(p3), sg.grid[p] != sg.grid.get(p4))
+                )
+
+        solved_grid, solution = solve(sg)
+        for p in sg.grid:
+            if p not in puzzle.symbols:
+                symbol = symbols[solved_grid[p]]
+                solution.symbols[p] = Symbol(symbol.style, symbol.shape.replace("_B", "_E"))
